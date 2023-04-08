230f79ef39bc195a016af671f6deef9c424747ce
Smit babariya <113500028+smit11001@users.noreply.github.com>
2023-04-08 20:29:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/animal_biography_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animal_biography_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/custom_app_bar.dart
@@ -0,0 +1,44 @@
+import 'package:animal_biography_ui/utils/strings.dart';
+import 'package:animal_biography_ui/utils/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+
+  final double opacity;
+
+  CustomAppBar({Key , this.opacity = 0.8}) : super(key:   Key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: opacity,
+      child: Container(
+        padding: const EdgeInsets.only(top: 48, left: 16, right: 16),
+        child: Row(
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: Strings.APP_NAME,
+                    style: TextStyles.appNameTextStyle,
+                  ),
+                  TextSpan(text: "\n"),
+                  TextSpan(
+                    text: Strings.TAG_LINE,
+                    style: TextStyles.tagLineTextStyle,
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
